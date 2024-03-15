209c9121d56385a9c64b94bad79ad473bfea2071
Bitos88 <tyrion72@gmail.com>
2024-03-15 19:55:21 +0100

    Initial Commit

---
+++ MangaVault/MangaVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaVaultApp.swift
+//  MangaVault
+//
+//  Created by Alberto Alegre Bravo on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MangaVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
